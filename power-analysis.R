913de1b8b02ae2360f49927e78ec128e1b88f898
Francisca <frcastrog@uc.cl>
2024-08-22 19:38:45 +0200

    power analysis update

--- power-analysis.R
+++ power-analysis.R
@@ -34,15 +34,13 @@ power_values <- sapply(sample_sizes, function(n) {
   pwr_result <- pwr.2p.test(h = effect_size_h, n = n, 
                             sig.level = alpha, power = NULL, 
                             alternative = "two.sided")
-  pwr_result$power
-})
+  pwr_result$power})
 
 # Data frame for plotting
 power_data <- data.frame(
   SampleSize = sample_sizes,
   Power = power_values)
 
-# Plot the power curve
 power_plot_h1 <- ggplot(power_data, aes(x = SampleSize, y = Power)) +
   geom_line() +
   geom_hline(yintercept = desired_power, linetype = "dashed", color = "red") +
@@ -55,8 +53,7 @@ power_plot_h1
 ggsave("outputs/power_plot_h1.png", 
        width = 11, height = 7, units = "cm", dpi = 600)
 
-
-# Calculate required sample size for each group to achieve desired power
+# Sample size for each group 
 total_sample_size_h1 <- pwr.2p.test(h = effect_size_h, sig.level = alpha, 
                                     power = desired_power, 
                                     alternative = "two.sided")$n
@@ -69,7 +66,6 @@ total_sample_size_h1 # 56.59031
 # Parameters 
 effect_size_d <- 0.70  
 
-# Power analysis for a range of sample sizes
 power_values_2 <- sapply(sample_sizes, function(n) {
   pwr_result <- pwr.t.test(d = effect_size_d, n = n, 
                            sig.level = alpha, power = NULL, 
@@ -94,14 +90,13 @@ power_plot_h1_continuous
 ggsave("outputs/power_plot_h1_continuous.png", 
        width = 11, height = 7, units = "cm", dpi = 600)
 
-# Calculate required sample size for each group to achieve desired power
+# Sample size for each group 
 total_sample_size_h1_continuous <- pwr.t.test(d = effect_size_d, sig.level = alpha, 
                                               power = desired_power, 
                                               type = "two.sample", 
                                               alternative = "two.sided")$n
 
 total_sample_size_h1_continuous #54.01938
-
 
 
 ## H2b/3a: effect of individual-elite congruence and elite-group incongruence ##
@@ -125,12 +120,12 @@ power_values_h2b <- sapply(sample_sizes_h2b, function(n) {
                                 alternative = "two.sided")
   pwr_result_h2b$power})
 
-# Create a data frame for plotting H2b
+# Data frame for plotting H2b
 power_data_h2b <- data.frame(
   SampleSize = sample_sizes_h2b,
   Power = power_values_h2b)
 
-# Plot the power curve for H2b
+# Plot the power curve 
 power_plot_h2b <- ggplot(power_data_h2b, aes(x = SampleSize, y = Power)) +
   geom_line() +
   geom_hline(yintercept = desired_power_h2b, linetype = "dashed", color = "red") +
@@ -143,7 +138,7 @@ power_plot_h2b
 ggsave("outputs/power_plot_h2b.png", power_plot_h2b, 
        width = 11, height = 7, units = "cm", dpi = 600)
 
-# Calculate required sample size per group for H2b
+# Sample size per
 sample_size_per_group_h2b <- pwr.2p.test(h = effect_size_h_h2b, sig.level = alpha_h2b, 
                                          power = desired_power_h2b, alternative = "two.sided")$n
 
@@ -163,20 +158,17 @@ sd_h2b <- 2.5         # SD of responses
 # Calculate Cohen's d for eval_3
 effect_size_d_h2b <- mean_diff_h2b / sd_h2b
 
-# Power analysis for a range of sample sizes for eval_3
 power_values_eval3_h2b <- sapply(sample_sizes_h2b, function(n) {
   pwr_result_h2b <- pwr.t.test(d = effect_size_d_h2b, n = n, 
                                sig.level = alpha_h2b, power = NULL, 
                                type = "two.sample", alternative = "two.sided")
   pwr_result_h2b$power})
 
-# Create a data frame for plotting eval_3
+# Create a data frame for plotting 
 power_data_eval3_h2b <- data.frame(
   SampleSize = sample_sizes_h2b,
-  Power = power_values_eval3_h2b
-)
+  Power = power_values_eval3_h2b)
 
-# Plot the power curve for eval_3
 power_plot_eval3_h2b <- ggplot(power_data_eval3_h2b, aes(x = SampleSize, y = Power)) +
   geom_line() +
   geom_hline(yintercept = desired_power_h2b, linetype = "dashed", color = "red") +
@@ -186,11 +178,10 @@ power_plot_eval3_h2b <- ggplot(power_data_eval3_h2b, aes(x = SampleSize, y = Pow
 
 power_plot_eval3_h2b
 
-# Save and print the plot for eval_3
 ggsave("outputs/power_plot_eval3_h2b.png", power_plot_eval3_h2b, 
        width = 11, height = 7, units = "cm", dpi = 600)
 
-# Calculate the required sample size per group for eval_3
+# Calculate sample size
 sample_size_per_group_h2b_eval3 <- pwr.t.test(d = effect_size_d_h2b, sig.level = alpha_h2b, 
                                               power = desired_power_h2b, 
                                               type = "two.sample", 
@@ -198,6 +189,58 @@ sample_size_per_group_h2b_eval3 <- pwr.t.test(d = effect_size_d_h2b, sig.level =
 
 
 sample_size_per_group_h2b_eval3 #73.16746
+
+
+########################## H2a/3b: interaction effects #########################
+
+### Binary outcomes: eval_1 and eval_2 ###
+
+# Parameters
+power_h2a_h3b <- power_interaction(
+  n.iter = 1000,                    # n of simulations
+  N = seq(200, 600, by = 10),       # sample size
+  r.x1.y = 0.2,                     # corr individual-elite congruence (x1) and candidate eval (y)
+  r.x2.y = 0.1,                     # corr elite-mobilized group congruence (x2) and candidate eval (y)
+  r.x1x2.y = 0.15,                  # corr interaction of individual-elite congruence and elite-mobilized group congruence (x1*x2) and candidate eval (y)
+  r.x1.x2 = 0.2,                    # corr between individual-elite congruence (x1) and elite-mobilized group congruence (x2)
+  k.x1 = 2,                         # values for individual-elite congruence (x1) (binary)
+  k.x2 = 2,                         # values for elite-mobilized group congruence (x2) (binary)
+  k.y = 2,                          # values for the evaluation of the candidate (y) (binary)
+  adjust.correlations = TRUE,      
+  alpha = 0.05)
+
+print(power_h2a_h3b)
+
+power_h2a_h3b <- plot_power_curve(power_h2a_h3b, power_target = .95)
+
+ggsave("outputs/power_h2a_h3b.png", 
+       width = 11, height = 7, units = "cm", dpi = 600)
+
+
+### Continuous outcomes: eval_3 ###
+
+power_h2a_h3b_eval3 <- power_interaction(
+  n.iter = 1000,                   # n of simulations
+  N = seq(200, 600, by = 10),      # sample size
+  r.x1.y = 0.2,                    # corr individual-elite congruence (x1) and candidate eval (y)
+  r.x2.y = 0.1,                    # corr elite-mobilized group congruence (x2) and candidate eval (y)
+  r.x1x2.y = 0.15,                 # corr interaction of individual-elite congruence and elite-mobilized group congruence (x1*x2) and candidate eval (y)
+  r.x1.x2 = 0.2,                   # corr individual-elite congruence (x1) and elite-mobilized group congruence (x2)
+  k.x1 = 2,                        # Values for individual-elite congruence (x1) (binary)
+  k.x2 = 2,                        # Values for elite-mobilized group congruence (x2) (binary)
+  k.y = 0,                         # Continuous evaluation of the candidate (y) (continuous)
+  adjust.correlations = TRUE,      
+  alpha = 0.05                    
+)
+
+print(power_h2a_h3b_eval3)
+
+power_h2a_h3b_eval3_plot <- plot_power_curve(power_h2a_h3b_eval3, power_target = .95)
+
+ggsave("outputs/power_h2a_h3b_eval3.png", 
+       plot = power_h2a_h3b_eval3_plot, 
+       width = 11, height = 7, units = "cm", dpi = 600)
+
 
 
 
